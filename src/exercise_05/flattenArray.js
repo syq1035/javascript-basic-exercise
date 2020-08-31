dc6ed5afcacea51cd3a24bbcfbbf31b193480f7b
syq <1035394794@qq.com>
2020-08-31 17:49:54 +0800

    [syq] add exercise_05

--- src/exercise_05/flattenArray.js
+++ src/exercise_05/flattenArray.js
@@ -6,5 +6,13 @@ export default function flattenArray(array) {
   // * Please implement this function and pass all the tests in flatten_array_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  if (!array) {
+    throw new Error('Flatten undefined or null array');
+  }
+
+  const result = array.reduce(
+    (pre, cur) => pre.concat(cur),
+    [],
+  );
+  return result;
 }
