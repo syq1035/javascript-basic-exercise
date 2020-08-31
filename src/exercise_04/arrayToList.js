1265835e300f61d96550b02f0b29e7ed21dbaf98
syq <1035394794@qq.com>
2020-08-31 17:27:10 +0800

    [syq] add exercise_04

--- src/exercise_04/arrayToList.js
+++ src/exercise_04/arrayToList.js
@@ -6,5 +6,26 @@ export default function arrayToList(array) {
   // * Please implement this function and pass all the tests in array_to_list_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  function Node(value) {
+    this.value = value;
+    this.next = null;
+  }
+
+  if (!array) {
+    throw new Error('Creating list from undefined array');
+  }
+  if (!array.length) {
+    throw new Error('Creating list from empty array');
+  }
+
+  const linkList = new Node(array[0]);
+  for (let idx = 1; idx < array.length; idx += 1) {
+    const node = new Node(array[idx]);
+    let p = linkList;
+    while (p.next != null) {
+      p = p.next;
+    }
+    p.next = node;
+  }
+  return linkList;
 }
