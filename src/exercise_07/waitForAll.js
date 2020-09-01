cb24c02fa864559f8f3045c61e41b1d876c4bc79
syq <1035394794@qq.com>
2020-09-01 17:58:01 +0800

    [syq] remove unimportant code

--- src/exercise_07/waitForAll.js
+++ src/exercise_07/waitForAll.js
@@ -9,12 +9,9 @@ export default function waitForAll(...promises) {
   // * Please implement this function and pass all the tests in wait_for_all_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  if (promises.some(itm => !(itm instanceof Promise))) {
-    throw new Error('Not all elements are promises.');
-  }
-
   const resolvedPromisesArray = [];
   let catchError = false;
+
   promises.forEach((p) => {
     if (!(p instanceof Promise)) {
       throw new Error('Not all elements are promises.');
@@ -23,6 +20,7 @@ export default function waitForAll(...promises) {
       catchError = true;
     }));
   });
+
   return Promise.all(resolvedPromisesArray)
     .then(() => (catchError ? Promise.reject() : Promise.resolve()));
 }
